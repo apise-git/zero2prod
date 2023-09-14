a9370d36e6eaa672b501257c20a8276d8bc97eff
Andre Pires <apise@vestas.com>
2023-09-14 11:15:03 +0100

    Project skeleton

---
+++ target/debug/deps/zero2prod-abc2568945a37ed6.d
@@ -0,0 +1,5 @@
+/home/apise/zero2prod/target/debug/deps/zero2prod-abc2568945a37ed6.rmeta: src/main.rs
+
+/home/apise/zero2prod/target/debug/deps/zero2prod-abc2568945a37ed6.d: src/main.rs
+
+src/main.rs:
