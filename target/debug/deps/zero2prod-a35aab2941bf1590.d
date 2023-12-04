fcf112ee456299a37a21fd71c2f197434bff008e
Andre Pires <apise@vestas.com>
2023-12-04 13:29:14 +0000

    full project + digital ocean

--- target/debug/deps/zero2prod-a35aab2941bf1590.d
+++
@@ -1,5 +0,0 @@
-/home/apise/zero2prod/target/debug/deps/zero2prod-a35aab2941bf1590.rmeta: src/main.rs
-
-/home/apise/zero2prod/target/debug/deps/zero2prod-a35aab2941bf1590.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/zero2prod-abc2568945a37ed6.d
+++
@@ -1,5 +0,0 @@
-/home/apise/zero2prod/target/debug/deps/zero2prod-abc2568945a37ed6.rmeta: src/main.rs
-
-/home/apise/zero2prod/target/debug/deps/zero2prod-abc2568945a37ed6.d: src/main.rs
-
-src/main.rs:
